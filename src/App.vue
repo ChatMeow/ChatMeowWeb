5e2f881d1e5b144531fea384411256716b24fa1c
MeowKJ <ijink@qq.com>
2023-02-03 00:36:24 +0800

    增加首页图片，优化包

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,23 @@
+<!--
+ * @Author: MeowKJ
+ * @Date: 2023-02-02 19:47:36
+ * @LastEditors: MeowKJ ijink@qq.com
+ * @LastEditTime: 2023-02-02 20:58:20
+ * @FilePath: /ChatMeowc:/Users/ijink/Project/Ai/chatmeowui/src/App.vue
+-->
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- 感谢API提供 拉菲艾拉La_Pluma-->
+  <img alt="" src="https://www.adorable0v0.top/api">
+  <HomePage/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HomePage from './components/HomePage.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HomePage
   }
 }
 </script>
@@ -21,6 +29,10 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 20px;
+}
+
+img{
+  height: 200px;
 }
 </style>
